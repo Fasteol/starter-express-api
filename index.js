631363f6f5101724109c2a991183ba4bf4c4e26b
Razan Muhammad <razanmfs507@gmail.com>
2024-03-17 20:55:29 +0700

    dsladlskjdsa

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ app.use(express.json());
 
 const filePath = "./data/db.json";
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 // Endpoint untuk mendapatkan semua data blog
 app.get("/blogs", async (req, res) => {
   try {
