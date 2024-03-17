2cdca3bd94a86b0cf2335a62817cdf1d47dfa476
Razan Muhammad <razanmfs507@gmail.com>
2024-03-17 21:57:32 +0700

    dsakjdlkas

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ app.use(
   })
 );
 app.use(express.json());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 const filePath = "./data/db.json";
 
