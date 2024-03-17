b62572c5b96bce8c78f949c376740f7f6730f718
Razan Muhammad <razanmfs507@gmail.com>
2024-03-17 22:19:47 +0700

    dsakdjlsak

--- index.js
+++ index.js
@@ -59,6 +59,36 @@ app.put("/blogs/:id", async (req, res) => {
   }
 });
 
+// Endpoint untuk menambahkan blog baru
+app.post("/blogs", async (req, res) => {
+  try {
+    const data = await fs.readFile(filePath);
+    let jsonData = JSON.parse(data);
+
+    // Mendapatkan data blog baru dari body request
+    const newBlog = req.body;
+
+    // Mendapatkan ID terakhir dari array blogs untuk menentukan ID baru
+    const lastBlog = jsonData.blogs[jsonData.blogs.length - 1];
+    const newId = lastBlog ? lastBlog.id + 1 : 1;
+
+    // Menambahkan ID ke blog baru
+    newBlog.id = newId;
+
+    // Menambahkan blog baru ke dalam array blogs
+    jsonData.blogs.push(newBlog);
+
+    // Menulis kembali data JSON ke file
+    await fs.writeFile(filePath, JSON.stringify(jsonData, null, 2));
+
+    // Mengirimkan respons dengan data blog baru yang ditambahkan
+    res.status(201).json(newBlog);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
 // Endpoint untuk menghapus blog berdasarkan id
 app.delete("/blogs/:id", async (req, res) => {
   const id = req.params.id;
